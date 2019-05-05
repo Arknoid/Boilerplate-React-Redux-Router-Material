1360bab8c4378db54e1d1d13741b9709b52f0bb4
arknoid 80 <omalige@gmail.com>
2019-05-05 11:53:54 +0200

    add eslint and  prettier

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      boilderplate-react-redux-router-material
+      boilderplagte-react-redux-router-material
     </div>
   );
 }
