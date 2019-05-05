3294a92a9dc3e6725d405550936079dd28bf6cef
arknoid 80 <omalige@gmail.com>
2019-05-05 15:17:14 +0200

    Add material ui

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function Dashboard(props) {
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
   return (
-    <div>
-      boilderplagte-react-redux-router-material 
+    <div className={classes.root}>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Boilderplate-react-redux-router-material
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
-  )
+  );
 }
 
-Dashboard.propTypes = {
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
-}
-
-export default Dashboard
-
+export default withStyles(styles)(ButtonAppBar);
